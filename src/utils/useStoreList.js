e1d59b4a5b90a0f0f61159149ac61113b57e1ac6
Ram Jammula <ramchowdary0308@gmail.com>
2024-09-06 20:55:58 -0400

    restaurent list api changes

--- src/utils/useStoreList.js
+++ src/utils/useStoreList.js
@@ -10,7 +10,7 @@ const [restaurantList, setRestaurentList] = useState([]);
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5169014&lng=78.3428304&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
     console.log(json);
-    setRestaurentList(json?.data?.cards[3].card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setRestaurentList(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
  }
 
     return restaurantList;
